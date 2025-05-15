25a4968ded83279d2b01cead3c52b8c8ff050609
Phulchand Kumar <phulchandkr7715@gmail.com>
2025-05-15 21:55:24 +0530

    Backend Completed!!

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+const { text } = require("express");
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
+  Comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date : {
+         type : Date,
+         default : Date.now
+      }
+    },
+  ],
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
